73f6cc7131f4d9a8dd0f86bac92b8317dbeb1824
JerezM <jerez149@gmail.com>
2021-08-05 20:23:37 -0300

    csrf token generation added to the security configuration

--- src/main/java/com/jerezm/springsecuritypractice/security/ApplicationSecurityConfig.java
+++ src/main/java/com/jerezm/springsecuritypractice/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.jerezm.springsecuritypractice.security.ApplicationUserRole.*;
 
@@ -30,6 +31,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
             .authorizeRequests()
             .antMatchers("/", "index").permitAll()            
             .anyRequest()
