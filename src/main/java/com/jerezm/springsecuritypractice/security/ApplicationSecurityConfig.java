e7ace984687a59b7249a0636aed777f595e486cc
JerezM <jerez149@gmail.com>
2021-08-09 11:31:03 -0300

    logout customization added to the http security on the ApplicationSecurityConfig

--- src/main/java/com/jerezm/springsecuritypractice/security/ApplicationSecurityConfig.java
+++ src/main/java/com/jerezm/springsecuritypractice/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 // import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static com.jerezm.springsecuritypractice.security.ApplicationUserRole.*;
 
@@ -47,7 +48,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .rememberMe()
                 .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-                .key("some-key-very-secure");
+                .key("some-key-very-secure")
+            .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))//when csrf is enable, remove this line
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
             
     }
 
