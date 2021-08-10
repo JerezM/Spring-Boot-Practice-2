17401d361e39b2d429796e6fc101c42588273d05
JerezM <jerez149@gmail.com>
2021-08-10 14:13:52 -0300

    JwtUsernameAndPasswordAuthenticationFilter added

---
+++ src/main/java/com/jerezm/springsecuritypractice/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package com.jerezm.springsecuritypractice.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, 
+                                                HttpServletResponse response) throws AuthenticationException {
+    
+        Authentication authenticate;
+        try {
+
+            UsernameAndPasswordAuthenticationRequest authenticationRequest = 
+                new ObjectMapper()
+                    .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getUsername(),
+                authenticationRequest.getPassword()
+            );
+
+            authenticate = this.authenticationManager.authenticate(authentication);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return authenticate;
+    }
+    
+}
