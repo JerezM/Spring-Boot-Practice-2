5d424b64236e72a0648762dc4d14cbb5026fa09d
JerezM <jerez149@gmail.com>
2021-08-11 18:10:07 -0300

    JwtTokenVerifier filter added to the ApplicationSecurityConfig after the JwtUsernameAndPasswordAuthenticationFilter

--- src/main/java/com/jerezm/springsecuritypractice/security/ApplicationSecurityConfig.java
+++ src/main/java/com/jerezm/springsecuritypractice/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 // import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import com.jerezm.springsecuritypractice.auth.ApplicationUserService;
+import com.jerezm.springsecuritypractice.jwt.JwtTokenVerifier;
 import com.jerezm.springsecuritypractice.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 
 @Configuration
@@ -41,6 +42,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()  
             .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(this.authenticationManager()))
+            .addFilterAfter(new JwtTokenVerifier(), JwtUsernameAndPasswordAuthenticationFilter.class)
             .authorizeRequests()
             .antMatchers("/", "index").permitAll()            
             .anyRequest()
